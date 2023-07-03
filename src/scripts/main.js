f3e92a9a4519a13f5a5c141e353624d91764923a
Mell789 <stillhere8209@gmail.com>
2023-07-03 15:11:32 +0300

    feat: mainjs added

---
+++ src/scripts/main.js
@@ -0,0 +1,35 @@
+const adviceCaption = document.querySelector(".adviceCaption");
+const adviceContext = document.querySelector(".adviceContext");
+const fetchBtn = document.querySelector(".fetchBtn");
+
+const api_link = "https://api.adviceslip.com/advice";
+
+const fetchNewAdvice = async () => {
+    const response = await fetch(api_link);
+    const data = await response.json();
+    return data;
+}
+
+const renderAdvice = (adviceObject) => {
+    const {id,advice} = adviceObject;
+    adviceCaption.textContent = `ADVICE #${id}`;
+    adviceContext.textContent =  advice;
+}
+
+const generateNewAdvice = async () => {
+    const data = await fetchNewAdvice();
+    const advice = data.slip;
+    renderAdvice(advice);
+    disableBtn();
+}
+
+const disableBtn = () => {
+    fetchBtn.classList.add("disabled");
+    setTimeout(() => {
+        fetchBtn.classList.remove("disabled");
+    }, 2000);
+}
+
+window.addEventListener("DOMContentLoaded",() => {
+    fetchBtn.addEventListener("click",generateNewAdvice);
+}); 
