e29892d7e563755269beb1d89d5c681324575a53
Mell789 <stillhere8209@gmail.com>
2023-07-03 16:18:45 +0300

    disable mainjs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,37 +1,37 @@
-const adviceCaption = document.querySelector(".adviceCaption");
-const adviceContext = document.querySelector(".adviceContext");
-const fetchBtn = document.querySelector(".fetchBtn");
+// const adviceCaption = document.querySelector(".adviceCaption");
+// const adviceContext = document.querySelector(".adviceContext");
+// const fetchBtn = document.querySelector(".fetchBtn");
 
-const api_link = "https://api.adviceslip.com/advice";
+// const api_link = "https://api.adviceslip.com/advice";
 
-const fetchNewAdvice = async () => {
-    const response = await fetch(api_link);
-    const data = await response.json();
-    return data;
-}
+// const fetchNewAdvice = async () => {
+//     const response = await fetch(api_link);
+//     const data = await response.json();
+//     return data;
+// }
 
-const renderAdvice = (adviceObject) => {
-    const {id,advice} = adviceObject;
-    adviceCaption.textContent = `ADVICE #${id}`;
-    adviceContext.textContent =  advice;
-}
+// const renderAdvice = (adviceObject) => {
+//     const {id,advice} = adviceObject;
+//     adviceCaption.textContent = `ADVICE #${id}`;
+//     adviceContext.textContent =  advice;
+// }
 
-const generateNewAdvice = async () => {
-    const data = await fetchNewAdvice();
-    const advice = data.slip;
-    renderAdvice(advice);
-    disableBtn();
-}
+// const generateNewAdvice = async () => {
+//     const data = await fetchNewAdvice();
+//     const advice = data.slip;
+//     renderAdvice(advice);
+//     disableBtn();
+// }
 
-const disableBtn = () => {
-    fetchBtn.disabled = true;
-    fetchBtn.classList.add("disabled");
-    setTimeout(() => {
-        fetchBtn.disabled = false;
-        fetchBtn.classList.remove("disabled");
-    }, 2000);
-}
+// const disableBtn = () => {
+//     fetchBtn.disabled = true;
+//     fetchBtn.classList.add("disabled");
+//     setTimeout(() => {
+//         fetchBtn.disabled = false;
+//         fetchBtn.classList.remove("disabled");
+//     }, 2000);
+// }
 
-window.addEventListener("DOMContentLoaded",() => {
-    fetchBtn.addEventListener("click",generateNewAdvice);
-}); 
+// window.addEventListener("DOMContentLoaded",() => {
+//     fetchBtn.addEventListener("click",generateNewAdvice);
+// }); 
